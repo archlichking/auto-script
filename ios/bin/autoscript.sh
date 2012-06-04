21161f9b9e041af78661b20055b9c37a0fdb66e7
Vincent Xie [Xie WeiShen] <vincent.xie@gree.co.jp>
2012-06-04 11:29:13 +0800

    fix a logic issue

--- ios/bin/autoscript.sh
+++ ios/bin/autoscript.sh
@@ -1,19 +1,19 @@
 #coding=utf-8
 
 APP_LOCATION="/Users/thunderzhulei/Library/Application Support/iPhone Simulator/"
-if [ -z $1 ]
+if [ ! -z $1 ]
 then
   APP_LOCATION=$1
 fi
 
 APP_NAME="OFQAJenkins"
-if [ -z $2 ]
+if [ ! -z $2 ]
 then
   APP_NAME=$2
 fi
 
 IOS_VERSION="5.1"
-if [ -z $3 ]
+if [ ! -z $3 ]
 then
   IOS_VERSION=$3
 fi
