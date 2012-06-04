d23299c41cf3207c763d9264bf491e5b11936c3a
Vincent Xie [Xie WeiShen] <vincent.xie@gree.co.jp>
2012-06-04 11:24:07 +0800

    Add parameter checking

--- android/bin/LaunchAuto.sh
+++ android/bin/LaunchAuto.sh
@@ -2,9 +2,33 @@
 # Added by Vincent at 2012/5/31
 #
 
+# ========== Config ==================
+
 avd_name="avd_2.3.3"
 apk_path="/Users/vincentxie/OpenFeint/ggpautomation/android/GGPClient-Android/bin/GGPClient-Automation.apk"
 activity="com.openfeint.qa.ggp/.DailyRunActivity"
+
+# ========== Init Parameter ==========
+# params:
+#       $1 : avd name
+#       $2 : apk name
+#       $3 : activity path
+if [ ! -z $1 ]
+then
+  avd_name=$1
+fi
+
+if [ ! -z $2 ]
+then
+  apk_path=$2
+fi
+
+if [ ! -z $3 ]
+then
+  activity=$3
+fi
+
+# ========== Action Begin ==========
 
 #Launch avd
 echo "Starting emulator...."
